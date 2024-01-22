c6a5c08d34cfcd21679d442015219dc36a9cdffa
Helmut Kohl <alig63301@gmail.com>
2024-01-22 23:17:28 +0000

    Started refactoring day 2 since it's a bit more involved than I though

--- src/com/aoc/DayTwo.java
+++ src/com/aoc/DayTwo.java
@@ -12,15 +12,26 @@ public class DayTwo {
 	
 	static final String PATH = "C:\\Users\\alig6\\eclipse-workspace\\AdventOfCode2023\\res\\dayTwo.txt";
 	static BufferedReader inputStream = null;
-	static final Set<String> numbers;
+	static final Set<String> numbers= new HashSet<>();
 	static {
-		numbers = new HashSet<>();
 		for (int i = 0; i < 10; i++) {
 			numbers.add(Integer.toString(i));
 		}
 	}
+	static final Set<String> colours = new HashSet<>();
+	static {
+		colours.add("Red"); colours.add("Green"); colours.add("Blue");
+	}
+	static enum COLOUR{
+		RED,
+		GREEN,
+		BLUE
+	}
+	
 	static final int[] limits = {12, 13, 14};
 	static Map<Integer, Boolean> result = new HashMap<>();
+	static int counter = 0;
+	static String current = "";
 	
 	
 	public static void run() throws IOException {
@@ -32,11 +43,12 @@ public class DayTwo {
 			int[] maxes = {};
 			
 			while ((line = inputStream.readLine()) != null) {
+				counter = 0;
+				current = line.substring(counter, counter + 1);
 				line = line.substring(5); // Strip out 'Game: '
 				id = parseNum(line);
-				System.out.println("id " + id);
-				
-				
+				System.out.println("id " + id);			
+				int[] lineMaxes = parseGame(line);			
 			}
 			
 		} finally {
@@ -47,8 +59,36 @@ public class DayTwo {
 
 	}
 	
+	private static int[] parseGame(String line) {
+		int[] result = {};
+		
+		while (!current.equals("\n")) {			
+			int[] drawResult;
+			if (current.equals(";")) {
+				drawResult = parseDraw(line);
+			} 
+			
+			if (counter < line.length()) {
+				counter++;
+			}
+		}
+		
+		return result;
+	}
+	
+	private static int[] parseDraw(String line) {
+		int[] drawResult = {};
+		Map<COLOUR, Integer> colorResult = new HashMap<>();
+		
+		while (!current.equals(";") && counter < line.length()) {
+			colourResult = parseColour();
+		}
+		
+		
+		return drawResult;
+	}
+	
 	private static int parseNum(String line) {
-		int counter = 0;
 		String testString = "-1";
 		
 		while (isWordNumber(Character.toString(line.charAt(counter)))) {
@@ -59,11 +99,41 @@ public class DayTwo {
 		return Integer.parseInt(testString);
 	}
 	
+	private static Map<COLOUR, Integer> parseColour(String line){
+		Map<COLOUR, Integer> result = new HashMap<>();
+		result.put(COLOUR.RED, 0);
+		result.put(COLOUR.GREEN, 0);
+		result.put(COLOUR.BLUE, 0);
+		
+		int quantity = parseNum(line);
+		int secondCounter = counter;
+		String testString = line.substring(secondCounter, secondCounter + 1);
+		
+		while (isColour(testString)) {
+			if (secondCounter < line.length() - 1) {
+				secondCounter++;
+			}	
+		}
+		
+		return result;
+	}
+	
 	private static boolean isWordNumber(String input) {
 		if (input != null && input.length() > 0) {
 			return numbers.contains(input) || input.isBlank();
 		}
 		return false;
 	}
+	
+	private static boolean isLetter(String input) {
+		return Character.isAlphabetic(input.toCharArray()[0]);
+	}
 
+	private static boolean isColour(String input) {
+		return colours.stream().anyMatch(colour -> (colour.startsWith(input)));
+	}
+	
+	private static String peek(String line) {
+		return line.substring(counter, counter+1);
+	}
 }
